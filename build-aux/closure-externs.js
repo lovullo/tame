696a5ab371834c7f70c1d824e32722e64c793aae
Mike Gerwitz <mike.gerwitz@ryansg.com>
2020-01-22 16:31:57 -0500

    build-aux/closure-externs.js: New file

---
+++ build-aux/closure-externs.js
@@ -0,0 +1,17 @@
+// externs for Closure Compiler
+var module = {
+    exports: {
+        rater: {
+            knownFields: {},
+            supplier: "",
+            meta: {},
+            consts: {},
+            params: {},
+            classify: {
+                desc: {},
+                fromMap: {},
+            },
+            fromMap: {},
+        },
+    },
+};
