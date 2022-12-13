d55b3add77c6a01c7868400631055fe414d9c743
Mike Gerwitz <mike.gerwitz@ryansg.com>
2022-12-13 13:57:04 -0500

    tamer: asg::air::test: Extract into own file
    Just minor preparatory work.
    
    DEV-13160

---
+++ tamer/src/asg/air/test.rs
@@ -0,0 +1,253 @@
+// Tests for ASG IR
+//
+//  Copyright (C) 2014-2022 Ryan Specialty Group, LLC.
+//
+//  This file is part of TAME.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//! These are tested as if they are another API directly atop of the ASG,
+//!   since that is how they are used.
+
+use std::assert_matches::assert_matches;
+
+use crate::{
+    asg::{Ident, Object},
+    parse::{ParseError, Parsed},
+};
+
+use super::*;
+
+type Sut = AirAggregate;
+
+#[test]
+fn ident_decl() {
+    let sym = "foo".into();
+    let kind = IdentKind::Tpl;
+    let src = Source {
+        src: Some("test/decl".into()),
+        ..Default::default()
+    };
+
+    let toks =
+        vec![AirToken::IdentDecl(sym, kind.clone(), src.clone())].into_iter();
+    let mut sut = Sut::parse(toks);
+
+    assert_eq!(Some(Ok(Parsed::Incomplete)), sut.next());
+
+    let asg = sut.finalize().unwrap().into_context();
+
+    let ident_node =
+        asg.lookup(sym).expect("identifier was not added to graph");
+    let ident = asg.get(ident_node).unwrap();
+
+    assert_eq!(
+        Ok(ident),
+        Ident::declare(sym)
+            .resolve(kind.clone(), src.clone())
+            .map(Object::Ident)
+            .as_ref(),
+    );
+
+    // Re-instantiate the parser and test an error by attempting to
+    //   redeclare the same identifier.
+    let bad_toks = vec![AirToken::IdentDecl(sym, kind, src)].into_iter();
+    let mut sut = Sut::parse_with_context(bad_toks, asg);
+
+    assert_matches!(
+        sut.next(),
+        Some(Err(ParseError::StateError(AsgError::IdentTransition(_)))),
+    );
+}
+
+#[test]
+fn ident_extern_decl() {
+    let sym = "foo".into();
+    let kind = IdentKind::Tpl;
+    let src = Source {
+        src: Some("test/decl-extern".into()),
+        ..Default::default()
+    };
+
+    let toks = vec![AirToken::IdentExternDecl(sym, kind.clone(), src.clone())]
+        .into_iter();
+    let mut sut = Sut::parse(toks);
+
+    assert_eq!(Some(Ok(Parsed::Incomplete)), sut.next());
+
+    let asg = sut.finalize().unwrap().into_context();
+
+    let ident_node =
+        asg.lookup(sym).expect("identifier was not added to graph");
+    let ident = asg.get(ident_node).unwrap();
+
+    assert_eq!(
+        Ok(ident),
+        Ident::declare(sym)
+            .extern_(kind, src.clone())
+            .map(Object::Ident)
+            .as_ref(),
+    );
+
+    // Re-instantiate the parser and test an error by attempting to
+    //   redeclare with a different kind.
+    let different_kind = IdentKind::Meta;
+    let bad_toks =
+        vec![AirToken::IdentExternDecl(sym, different_kind, src)].into_iter();
+    let mut sut = Sut::parse_with_context(bad_toks, asg);
+
+    assert_matches!(
+        sut.next(),
+        Some(Err(ParseError::StateError(AsgError::IdentTransition(_)))),
+    );
+}
+
+#[test]
+fn ident_dep() {
+    let ident = "foo".into();
+    let dep = "dep".into();
+
+    let toks = vec![AirToken::IdentDep(ident, dep)].into_iter();
+    let mut sut = Sut::parse(toks);
+
+    assert_eq!(Some(Ok(Parsed::Incomplete)), sut.next());
+
+    let asg = sut.finalize().unwrap().into_context();
+
+    let ident_node = asg
+        .lookup(ident)
+        .expect("identifier was not added to graph");
+    let dep_node = asg.lookup(dep).expect("dep was not added to graph");
+
+    assert!(asg.has_dep(ident_node, dep_node));
+}
+
+#[test]
+fn ident_fragment() {
+    let sym = "frag".into();
+    let kind = IdentKind::Tpl;
+    let src = Source {
+        src: Some("test/frag".into()),
+        ..Default::default()
+    };
+    let frag = "fragment text".into();
+
+    let toks = vec![
+        // Identifier must be declared before it can be given a
+        //   fragment.
+        AirToken::IdentDecl(sym, kind.clone(), src.clone()),
+        AirToken::IdentFragment(sym, frag),
+    ]
+    .into_iter();
+
+    let mut sut = Sut::parse(toks);
+
+    assert_eq!(Some(Ok(Parsed::Incomplete)), sut.next()); // IdentDecl
+    assert_eq!(Some(Ok(Parsed::Incomplete)), sut.next()); // IdentFragment
+
+    let asg = sut.finalize().unwrap().into_context();
+
+    let ident_node =
+        asg.lookup(sym).expect("identifier was not added to graph");
+    let ident = asg.get(ident_node).unwrap();
+
+    assert_eq!(
+        Ok(ident),
+        Ident::declare(sym)
+            .resolve(kind.clone(), src.clone())
+            .and_then(|resolved| resolved.set_fragment(frag))
+            .map(Object::Ident)
+            .as_ref(),
+    );
+
+    // Re-instantiate the parser and test an error by attempting to
+    //   re-set the fragment.
+    let bad_toks = vec![AirToken::IdentFragment(sym, frag)].into_iter();
+    let mut sut = Sut::parse_with_context(bad_toks, asg);
+
+    assert_matches!(
+        sut.next(),
+        Some(Err(ParseError::StateError(AsgError::IdentTransition(_)))),
+    );
+}
+
+// Adding a root before the identifier exists should add a
+//   `Ident::Missing`.
+#[test]
+fn ident_root_missing() {
+    let sym = "toroot".into();
+
+    let toks = vec![AirToken::IdentRoot(sym)].into_iter();
+    let mut sut = Sut::parse(toks);
+
+    assert_eq!(Some(Ok(Parsed::Incomplete)), sut.next());
+
+    let asg = sut.finalize().unwrap().into_context();
+
+    let ident_node = asg
+        .lookup(sym)
+        .expect("identifier was not added to the graph");
+    let ident = asg.get(ident_node).unwrap();
+
+    // The identifier did not previously exist,
+    //   and so a missing node is created as a placeholder.
+    assert_eq!(&Object::Ident(Ident::Missing(sym)), ident);
+
+    // And that missing identifier should be rooted.
+    assert!(asg.is_rooted(ident_node));
+}
+
+#[test]
+fn ident_root_existing() {
+    let sym = "toroot".into();
+    let kind = IdentKind::Tpl;
+    let src = Source {
+        src: Some("test/root-existing".into()),
+        ..Default::default()
+    };
+
+    // Ensure that it won't auto-root based on the kind,
+    //   otherwise we won't be testing the right thing.
+    assert!(!kind.is_auto_root());
+
+    let toks = vec![
+        AirToken::IdentDecl(sym, kind.clone(), src.clone()),
+        AirToken::IdentRoot(sym),
+    ]
+    .into_iter();
+
+    let mut sut = Sut::parse(toks);
+
+    assert_eq!(Some(Ok(Parsed::Incomplete)), sut.next()); // IdentDecl
+    assert_eq!(Some(Ok(Parsed::Incomplete)), sut.next()); // IdentRoot
+
+    let asg = sut.finalize().unwrap().into_context();
+
+    let ident_node = asg
+        .lookup(sym)
+        .expect("identifier was not added to the graph");
+    let ident = asg.get(ident_node).unwrap();
+
+    // The previously-declared identifier...
+    assert_eq!(
+        Ok(ident),
+        Ident::declare(sym)
+            .resolve(kind.clone(), src.clone())
+            .map(Object::Ident)
+            .as_ref()
+    );
+
+    // ...should have been subsequently rooted.
+    assert!(asg.is_rooted(ident_node));
+}
